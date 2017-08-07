8e8a11534f271254fdfbf037042bfac0c9b6e15e
Jay Welborn <jesse.welborn@gmail.com>
2017-08-07 09:38:27 +0200

    created model for code snippet

---
+++ drf_tutorial/snippets/models.py
@@ -0,0 +1,42 @@
+# django imports
+from django.db import models
+
+# 3rd-party imports
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+
+# define pygment stuff
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
+# Create your models here.
+
+class Snippet(models.Model):
+    """
+    Model for storing code snippets.
+
+    Attributes:
+        created(DateTimeField): date snippet was created
+        title(CharField): title of code snippet
+        code(TextField): field for housing the actual code
+        linenos(BooleanField): should the output display line numbers?
+        language(CharField): language for highlighting. default is python.
+        style(CharField): how to display code. defaults to friendly.
+    """
+
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, 
+                                default='python', 
+                                max_length=100)
+    style - models.CharField(choices=STYLE_CHOICES, 
+                             default='friendly', 
+                             max_length=100)
+
+    class Meta:
+        ordering = ('created',)
+        
+            
